d58f8d567e8c2cffb8f5a860143f113aba1dcfb3
Chris Hitchcott <hitchcott@gmail.com>
2014-06-11 17:17:06 +0800

    Integrated tap-i18n

--- package.js
+++ package.js
@@ -7,9 +7,17 @@ Package.on_use(function (api) {
     api.use('jquery', 'client');
     api.use('underscore', 'client');
     api.use('just-i18n', 'client');
+    api.use('tap-i18n', ['client','server']);
 
+    api.add_files("package-tap.i18n", ['client', 'server']);
     api.add_files('lib/reactive_table.html', 'client');
     api.add_files('lib/reactive_table_i18n.js', 'client');
     api.add_files('lib/reactive_table.js', 'client');
     api.add_files('lib/reactive_table.css', 'client');
+
+
+    api.add_files([
+      "i18n/en.i18n.json",
+      "i18n/zh.i18n.json",
+    ], 'client');
 });
